7abdd01bce4c5451daefaafd35c9f0722411697d
daw <toteskuu@gmail.com>
2019-04-04 19:58:18 +0100

    04-04-19

---
+++ app/src/main/java/com/ciscodiz/calculadoraandroid/controlador/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ciscodiz.calculadoraandroid.controlador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ciscodiz.calculadoraandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
